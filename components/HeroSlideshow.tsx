930b348640b383d90b51834329c4bada448133f2
elisha.sagoe <elishaatosagoe@gmail.com>
2026-09-05 23:05:53 +0000

    Center couple in hero slideshow on mobile and desktop viewports

--- components/HeroSlideshow.tsx
+++ components/HeroSlideshow.tsx
@@ -8,47 +8,47 @@ import type { SiteMedia } from '@/lib/types';
 const slides = [
   {
     src: '/hero/TBC5-72377c25-44f5-4487-90a6-32e6285327f6.jpg',
-    position: 'left top',
+    position: 'center 25%',
   },
   {
     src: '/hero/TBC140-9c898793-a28e-41bb-a3b2-c0f640100ae9.jpg',
-    position: 'left top',
+    position: 'center 25%',
   },
   {
     src: '/hero/TBC267-95769264-776e-42ff-8a97-d3d8e19b498f.jpg',
-    position: 'left top',
+    position: 'center 25%',
   },
   {
     src: '/hero/TBC77-1e1ed5ff-242f-42b1-82ab-3a5b0b0560b1.jpg',
-    position: 'left top',
+    position: 'center 25%',
   },
   {
     src: '/hero/TBC171-5be6d6b0-ceb7-45c3-a0ba-d7bb1c37a0d0.jpg',
-    position: 'right top',
+    position: 'center 25%',
   },
   {
     src: '/hero/TBC-90ef3dd1-4489-43a6-82e5-0b53e9280206.jpg',
-    position: 'center top',
+    position: 'center 25%',
   },
   {
     src: '/hero/TBC288-8c168739-6060-41c1-b5e3-93b0581aa660.jpg',
-    position: 'left top',
+    position: 'center 25%',
   },
   {
     src: '/hero/TBC84-49a49f9e-d683-41a7-97f8-a0905f77cd1f.jpg',
-    position: 'left top',
+    position: 'center 25%',
   },
   {
     src: '/hero/TBC244-423318dc-84da-459a-beab-47e134803a4d.jpg',
-    position: 'left top',
+    position: 'center 25%',
   },
   {
     src: '/hero/TBC17-9734aa80-3a09-4d7e-98d1-3059177cdaa4.jpg',
-    position: 'left top',
+    position: 'center 25%',
   },
   {
     src: '/hero/TBC235-3bba5f4b-b5a2-4db4-bf7a-19d3e72233d4.jpg',
-    position: 'right top',
+    position: 'center 25%',
   },
 ];
 
